a94a98a4ada8d90381235921e17a5d3e9c928e9d
Teddi Maull <teddi@picturetheday.com>
2015-02-04 21:10:41 -0700

    Fix gemfile and fix some typos

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'railties', '~> 4.1.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
-setTimeout(function() {
-  window.location = 'http://localh0st-3000.herokuapp.com/';
-}, 10000)
+// setTimeout(function() {
+//   window.location = 'http://localh0st-3000.herokuapp.com/';
+// }, 10000)
